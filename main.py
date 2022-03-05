f9591dad9784b2ae7796b535a46bc9f18627a6ab
FloatingComet62 <shubh2007rai@gmail.com>
2022-03-06 00:10:53 +0530

    Horizontal and Vertical Checking

--- main.py
+++ main.py
@@ -11,19 +11,55 @@ grid = [
     [blank, blank, blank, blank, blank, blank, blank]
 ]
 player = "X"
+running = True
 
-while True:
+while running:
     column = int(input("Which column?\n> "))
-    dropRow = ""
     os.system("cls")
     grid.reverse()
     for row in grid:
         if row[column - 1] == blank:
             row[column - 1] = player
-            dropRow = row
             break
     
     grid.reverse()
+
+    columnList = [
+        [grid[0][0], grid[1][0], grid[2][0], grid[3][0], grid[4][0], grid[5][0], grid[6][0]],
+        [grid[0][1], grid[1][1], grid[2][1], grid[3][1], grid[4][1], grid[5][1], grid[6][1]],
+        [grid[0][2], grid[1][2], grid[2][2], grid[3][2], grid[4][2], grid[5][2], grid[6][2]],
+        [grid[0][3], grid[1][3], grid[2][3], grid[3][3], grid[4][3], grid[5][3], grid[6][3]],
+        [grid[0][4], grid[1][4], grid[2][4], grid[3][4], grid[4][4], grid[5][4], grid[6][4]],
+        [grid[0][5], grid[1][5], grid[2][5], grid[3][5], grid[4][5], grid[5][5], grid[6][5]],
+        [grid[0][6], grid[1][6], grid[2][6], grid[3][6], grid[4][6], grid[5][6], grid[6][6]]
+    ]
+
+    for col in columnList:
+        for i in range(len(col) - 1):
+            try:
+                item = col[i]
+                nextItem = col[i + 1]
+                nextnextItem = col[i + 2]
+                nextnextnextItem = col[i + 3]
+                if item == player and nextItem == player and nextnextItem == player and nextnextnextItem == player:
+                    print("You win!")
+                    running = False
+            except:
+                pass
+    
+    for row in grid:
+        for i in range(len(row) - 1):
+            try:
+                item = row[i]
+                nextItem = row[i + 1]
+                nextnextItem = row[i + 2]
+                nextnextnextItem = row[i + 3]
+                if item == player and nextItem == player and nextnextItem == player and nextnextnextItem == player:
+                    print("You win!")
+                    running = False
+            except:
+                pass
+    
     
     if player == "X":
         player = "O"
