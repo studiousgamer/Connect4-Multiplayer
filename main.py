7e38c5a2254ce65b167494513b0adc619c2672c9
FloatingComet62 <shubh2007rai@gmail.com>
2022-03-06 00:49:08 +0530

    Diagonal Connect 4

--- main.py
+++ main.py
@@ -33,6 +33,25 @@ while running:
         [grid[0][5], grid[1][5], grid[2][5], grid[3][5], grid[4][5], grid[5][5], grid[6][5]],
         [grid[0][6], grid[1][6], grid[2][6], grid[3][6], grid[4][6], grid[5][6], grid[6][6]]
     ]
+    topLeftTobottomRight = [
+        [grid[3][0], grid[4][1], grid[5][2], grid[6][3]],
+        [grid[2][0], grid[3][1], grid[4][2], grid[5][3], grid[6][4]],
+        [grid[1][0], grid[2][1], grid[3][2], grid[4][3], grid[5][4], grid[6][5]],
+        [grid[0][0], grid[1][1], grid[2][2], grid[3][3], grid[4][4], grid[5][5], grid[6][6]],
+        [grid[0][1], grid[1][2], grid[2][3], grid[3][4], grid[4][5], grid[5][6]],
+        [grid[0][2], grid[1][3], grid[2][4], grid[3][5], grid[4][6]],
+        [grid[0][3], grid[1][4], grid[2][5], grid[3][6]],
+    ]
+    topRightTobottomLeft = [
+        [grid[0][3], grid[1][2], grid[2][1], grid[3][0]],
+        [grid[0][4], grid[1][3], grid[2][2], grid[3][1], grid[4][0]],
+        [grid[0][5], grid[1][4], grid[2][3], grid[3][2], grid[4][1], grid[5][0]],
+        [grid[0][6], grid[1][5], grid[2][4], grid[3][3], grid[4][2], grid[5][1], grid[6][0]],
+        [grid[5][0], grid[4][1], grid[3][2], grid[2][3], grid[1][4], grid[0][5]],
+        [grid[4][0], grid[3][1], grid[2][2], grid[1][3], grid[0][4]],
+        [grid[3][0], grid[2][1], grid[1][2], grid[0][3]],
+    ]
+
 
     for col in columnList:
         for i in range(len(col) - 1):
@@ -48,6 +67,32 @@ while running:
                 pass
     
     for row in grid:
+        for i in range(len(row) - 1):
+            try:
+                item = row[i]
+                nextItem = row[i + 1]
+                nextnextItem = row[i + 2]
+                nextnextnextItem = row[i + 3]
+                if item == player and nextItem == player and nextnextItem == player and nextnextnextItem == player:
+                    print("You win!")
+                    running = False
+            except:
+                pass
+    
+    for row in topLeftTobottomRight:
+        for i in range(len(row) - 1):
+            try:
+                item = row[i]
+                nextItem = row[i + 1]
+                nextnextItem = row[i + 2]
+                nextnextnextItem = row[i + 3]
+                if item == player and nextItem == player and nextnextItem == player and nextnextnextItem == player:
+                    print("You win!")
+                    running = False
+            except:
+                pass
+
+    for row in topRightTobottomLeft:
         for i in range(len(row) - 1):
             try:
                 item = row[i]
